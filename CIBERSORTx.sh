07595d2c45e7885cfed94cfab1515f4d09c2af53
Robert Schauner <schaunr@ccf.org>
2021-05-14 12:34:02 -0400

    update output directory

--- CIBERSORTx.sh
+++ CIBERSORTx.sh
@@ -1,45 +1,49 @@
 #! /bin/bash
 
-cd ~/Documents/Lab/clonal_evolution/biomarkers
+cd ~/Documents/Lab/clonal_evolution/biomarkers || exit
 
 TOKEN=e6ac1e482bcc1c8f22f80496aca50d86
 EMAIL=rds156@case.edu
 
+OUTDIR=$PWD"/outs/cibersort_results"
+SIGMAT=nonstem_clusters_sigmatrix.txt
+
 MIX=tcga_cibersort.txt
 docker run -v $PWD/cibersort_in:/src/data \
-  -v $PWD/cibersort_results:/src/outdir \
+  -v $OUTDIR:/src/outdir \
   cibersortx/fractions \
   --username $EMAIL \
   --verbose TRUE \
   --token $TOKEN \
   --single_cell TRUE \
-  --outdir $PWD/cibersort_results \
-  --sigmatrix nonstem_GEP.txt \
+  --outdir $OUTDIR \
+  --sigmatrix $SIGMAT \
   --mixture $MIX \
   --label tcga
   
 MIX=beat_aml.txt
 docker run -v $PWD/cibersort_in:/src/data \
-  -v $PWD/cibersort_results:/src/outdir \
+  -v $OUTDIR:/src/outdir \
   cibersortx/fractions \
   --username $EMAIL \
   --verbose TRUE \
   --token $TOKEN \
   --single_cell TRUE \
-  --outdir $PWD/cibersort_results \
-  --sigmatrix nonstem_GEP.txt \
+  --outdir $OUTDIR \
+  --sigmatrix $SIGMAT \
   --mixture $MIX \
   --label beatAML
   
 MIX=target_data.txt
 docker run -v $PWD/cibersort_in:/src/data \
-  -v $PWD/cibersort_results:/src/outdir \
+  -v $OUTDIR:/src/outdir \
   cibersortx/fractions \
   --username $EMAIL \
   --verbose TRUE \
   --token $TOKEN \
   --single_cell TRUE \
-  --outdir $PWD/cibersort_results \
-  --sigmatrix nonstem_GEP.txt \
+  --outdir $OUTDIR \
+  --sigmatrix $SIGMAT \
   --mixture $MIX \
   --label target
+
