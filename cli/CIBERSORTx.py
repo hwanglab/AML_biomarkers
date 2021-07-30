fbc336e3c974e1fee43edee28f163f02c7971df4
Robert Schauner <rds156@case.edu>
2021-07-30 13:59:16 -0400

    add invocation to python script

--- cli/CIBERSORTx.py
+++ cli/CIBERSORTx.py
@@ -3,6 +3,7 @@
 import subprocess
 import argparse
 import os
+import shutil
 
 parser = argparse.ArgumentParser(description = "Deconvolute Samples")
 
@@ -10,7 +11,7 @@ parser.add_argument("--dir", "-d", help="path to run directory", default=None)
 parser.add_argument("--ref", "-r", help="path to CIBERSORTx GEP", required=True)
 parser.add_argument("--id", "-i", help="ID to use for outputs", required=True)
 parser.add_argument("--cores", "-c", help="number of cores to use", default=1)
-parser.add_argument("--verbose", "-v", help="verbose level to use")
+parser.add_argument("--verbose", "-v", help="verbose level to use", default=2)
 parser.add_argument("--container-software", "-C", help="what container software to use", default="singularity")
 
 argv = parser.parse_args(["-r", "CIBERSORTx_cibersort_ref_input_inferred_refsample.txt", "-i", "flt3_cebpa"])
@@ -58,7 +59,34 @@ def run_cmd(mixture):
     flattened = [val for sublist in res for val in sublist]
     return flattened
 
-data = ["tcga_data.txt", "target_data.txt", "beat_aml"]
+data = ["tcga_data.txt", "target_data.txt", "beat_aml.txt"]
 
 for dat in data:
+    source_path = "{}/{}".format("cibersort_in", dat)
+    destination_path = "{}/{}".format(output_path, dat)
+    shutil.copyfile(source_path, destination_path)
     subprocess.run(run_cmd(dat))
+
+def write_invocation(argv, output_path):
+    import time
+    import sys
+    args = vars(argv).items()
+    args = list(args)
+    sys_cmd = sys.argv[0]
+
+    sys_time = time.asctime(time.localtime(time.time()))
+
+    header = "{}: {}------------------".format(sys_cmd, sys_time)
+
+    fname = output_path + "/invocation"
+    file1 = open(fname, "a")  # append mode
+    file1.write(header)
+    file1.write("\n")
+    for arg in args:
+        formatted_args = "{}: {}".format(arg[0], arg[1])
+        file1.write(formatted_args)
+        file1.write("\n")
+    file1.write("\n")
+    file1.close()
+ 
+write_invocation(argv, output_path)
