18db13f8876f262daa8d1f945a7b5f6fe27e90a0
Robert Schauner <rds156@case.edu>
2021-07-30 16:11:29 -0400

    Add reference creation and clean up logging

--- cli/CIBERSORTx.py
+++ cli/CIBERSORTx.py
@@ -12,13 +12,13 @@ parser.add_argument("--dir", "-d", help="path to run directory", default=None)
 parser.add_argument("--ref", "-r", help="path to CIBERSORTx GEP", required=True)
 parser.add_argument("--id", "-i", help="ID to use for outputs", required=True)
 parser.add_argument("--cores", "-c", help="number of cores to use", default=1)
-parser.add_argument("--verbose", "-v", help="verbose level to use", default=2)
+parser.add_argument("--verbose", "-v", help="verbose level to use [1 (DEBUG) - 5 (CRITICAL)]", default=2)
 parser.add_argument("--container-software", "-C", help="what container software to use", default="singularity")
 
-argv = parser.parse_args(["-r", "CIBERSORTx_cibersort_ref_input_inferred_refsample.txt", "-i", "flt3_cebpa"])
+argv = parser.parse_args()
 
 logger = logging.getLogger("CIBERSORTx")
-logger.setLevel(vars(argv).get("verbose"))
+logger.setLevel(vars(argv).get("verbose") * 10)
 
 docker_singularity_cmd = vars(argv).get("container_software")
 
@@ -44,9 +44,7 @@ name_of_output_directory = "/cibersort_results"
 # set up bind mounts
 input_bind = output_path + ":/src/data"
 output_bind = output_path + name_of_output_directory + ":/src/outdir"
-
-# Code to make reference here
-
+   
 user_email = os.getenv("EMAIL")
 user_token = os.getenv("TOKEN")
 
@@ -56,15 +54,26 @@ if use_singularity:
 else:
     container_cmd = "docker" + " " + "run" + "-v " + input_bind + "-v " + output_bind + " cibersortx/fractions"
 
-cibersort_cmd = ["--username", user_email, "--verbose", "FALSE", "--token", user_token, " --single_cell", "TRUE", "--outdir", output_path + name_of_output_directory]
+logger.debug("Testing if Reference can be found")
 
 ref_filename = vars(argv).get("ref")
+cibersort_cmd = ["--username", user_email, "--verbose", "FALSE", "--token", user_token, " --single_cell", "TRUE", "--outdir", output_path + name_of_output_directory]
+
+if os.path.isfile("{}/{}".format(output_path, ref_filename)):
+    logger.info("Existing Reference Found")
+else:
+    logger.info("Reference Not Found. Creating Reference")
+    ref_create_cmd = ["--refsample", "cibersort_ref_input.txt", "--fraction", "0.50"]
+    ref_run_cmd = [container_cmd, cibersort_cmd, ref_create_cmd]
+    ref_run_cmd = [val for sublist in ref_run_cmd for val in sublist]
+    subprocess.run(ref_run_cmd, stdout=subprocess.DEVNULL)
+    logger.info("Reference Creation Finished")
 
 def run_cmd(mixture):
-    specific_args = ["--sigmatrix", ref_filename, "--mixture", mixture]
+    specific_args = ["--sigmatrix", ref_filename, "--mixture", mixture, "--label", mixture]
     res = [container_cmd, cibersort_cmd, specific_args]
     flattened = [val for sublist in res for val in sublist]
-    return flattened + " > /dev/null"
+    return flattened
 
 data = ["tcga_data.txt", "target_data.txt", "beat_aml.txt"]
 
@@ -73,7 +82,7 @@ for dat in data:
     source_path = "{}/{}".format("cibersort_in", dat)
     destination_path = "{}/{}".format(output_path, dat)
     shutil.copyfile(source_path, destination_path)
-    subprocess.run(run_cmd(dat))
+    subprocess.run(run_cmd(dat), stdout=subprocess.DEVNULL)
 
 def write_invocation(argv, output_path):
     import time
