9791110f7fbe53caa60042cd861785509e2adf86
Robert Schauner <rds156@case.edu>
2021-08-02 17:11:53 -0400

    fix error for CIBERSORTx

--- lib/BeatAML_data_prep.R
+++ lib/BeatAML_data_prep.R
@@ -6,5 +6,4 @@ beat_aml <- read_tsv(here("data/aml_ohsu_2018/data_RNA_Seq_expression_cpm.txt"))
 
 rename(beat_aml, GeneSymbol = Hugo_Symbol) %>%
   mutate(Entrez_Gene_Id = NULL) %>%
-  transposeDF() %>%
   write_tsv(here("cibersort_in/beat_aml.txt"))
