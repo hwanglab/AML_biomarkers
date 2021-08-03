419ed1f54a0b893988e53da602aa338e2527f050
Robert Schauner <rds156@case.edu>
2021-08-03 13:27:41 -0400

    split CIBERSORTx reference creation

--- cli/CIBERSORTx_ref.py
+++ cli/CIBERSORTx_ref.py
@@ -14,26 +14,12 @@ parser.add_argument("--dir", "-d", help="path to run directory", default=None)
 parser.add_argument("--id", "-i", help="ID to use for outputs", required=True)
 parser.add_argument("--cores", "-c", help="number of cores to use", default=1)
 parser.add_argument("--verbose", "-v", help="verbose level to use [1 (DEBUG) - 5 (CRITICAL)]", default=2, type=int)
-parser.add_argument("--mixture", "-m", help="what mixture file to use", required=True, nargs="+")
-parser.add_argument("--batch-correct", help="Should B mode batch correction be applied?", action="store_true")
 parser.add_argument("--debug-cibersort", "-D", help="Should the max amount of information from CIBERSORT be printed?", action="store_true")
 
 argv = parser.parse_args()
 
-logging.basicConfig(level=vars(argv).get("verbose"), format="%(levelname)s [%(asctime)s] %(name)s %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
-
-def concatenate_list_data(list):
-    result= ''
-    for element in list:
-        result += str(element)
-        result += " "
-    return result
-if (len(vars(argv).get("mixture")) is 1):
-    name_to_use = vars(argv).get("mixture")[0]
-else:
-    name_to_use = concatenate_list_data(vars(argv).get("mixture"))
-
-logger = logging.getLogger("CIBERSORTx: [{}]".format(name_to_use))
+logging.basicConfig(level=vars(argv).get("verbose"), format="%(levelname)s [%(asctime)s] [%(name)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+logger = logging.getLogger("CIBERSORTx: Reference")
 
 docker_singularity_cmd = "docker"
 try: 
@@ -66,9 +52,9 @@ else:
     plots_path = vars(argv).get("dir") + "/plots/" + vars(argv).get("id")
 
 random_string = binascii.b2a_hex(os.urandom(15))
-name_of_output_directory = "/cibersort_results/{}".format(random_string)
+name_of_output_directory = "/cibersort_results/"
 
-logger.debug("Output Directory:{}".format(name_of_output_directory))
+logger.debug("Output Directory: {}".format(name_of_output_directory))
 
 try:
     os.mkdir(output_path + "/cibersort_results")
@@ -111,35 +97,18 @@ elif os.path.isfile("{}/cibersort_results/{}".format(output_path, ref_filename))
     destination_path = "{}/{}".format(output_path, ref_filename)
     shutil.copyfile(source_path, destination_path)
 else:
-    logger.info("Reference Not Found. Please run CIBERSORTx_ref.py")
-    raise RuntimeError("Reference Not Found")
-    
-def run_cmd(mixture):
-    specific_args = ["--sigmatrix", ref_filename, "--mixture", mixture, "--label", mixture, "--rmbatchBmode", str(vars(argv).get("batch_correct")).upper()]
-    res = [container_cmd, cibersort_cmd, specific_args]
-    flattened = [val for sublist in res for val in sublist]
-    return flattened
-
-data = vars(argv).get("mixture")
-
-for dat in data:
-    logger.info("Running CIBERSORTx on {}".format(dat))
-    source_path = "{}/{}".format("cibersort_in", dat)
-    destination_path = "{}/{}".format(output_path, dat)
-    shutil.copyfile(source_path, destination_path)
-    logger.debug("The run command is: {}".format(" ".join(run_cmd(dat))))
+    logger.info("Reference Not Found. Creating Reference")
+    if not os.path.isfile("{}/cibersort_ref_input.txt".format(output_path)):
+        raise FileNotFoundError("CIBERSORTx Reference Inputs not Found -- please run make_CIBERSORT_ref.R first")
+    ref_create_cmd = ["--refsample", "cibersort_ref_input.txt", "--fraction", "0.50"]
+    ref_run_cmd = [container_cmd, cibersort_cmd, ref_create_cmd]
+    ref_run_cmd = [val for sublist in ref_run_cmd for val in sublist]
     if vars(argv).get("debug_cibersort"):
         subprocess_fun = subprocess.STDOUT
     else:
         subprocess_fun = subprocess.DEVNULL
-    subprocess.run(run_cmd(dat), stdout=subprocess_fun)
-
-files = os.listdir("{}/cibersort_results/{}".format(output_path, random_string))
-
-for f in files:
-    source_path = "{}/cibersort_results/{}/{}".format(output_path, random_string, f)
-    destination_path = "{}/cibersort_results".format(output_path)
-    shutil.copyfile(source_path, destination_path)
+    subprocess.run(ref_run_cmd, stdout=subprocess_fun)
+    logger.info("Reference Creation Finished")
 
 def write_invocation(argv, output_path):
     import time
