6ada35f721e686e71ac4fd16fb339f879a3cbd78
Robert Schauner <rds156@case.edu>
2022-01-03 14:50:58 -0500

    add logging to prepare clinical info

--- cli/lib/prepare_clin_info.R
+++ cli/lib/prepare_clin_info.R
@@ -1,21 +1,20 @@
 library(here)
 library(readxl)
-library(xlsx)
 library(tidyverse)
 
 source(here("lib/functions.R"))
 ### TARGET ----
 
-debug(logger, "Preparing TARGET Data")
-debug(logger, "Reading Validation Data")
+if(exists("logger")) debug(logger, "Preparing TARGET Data")
+if(exists("logger")) debug(logger, "Reading Validation Data")
 val <- read_excel(
   here("clinical_info/TARGET_AML_ClinicalData_Validation_20181213.xlsx")
 )
-debug(logger, "Reading Discovery Data")
+if(exists("logger")) debug(logger, "Reading Discovery Data")
 dis <- read_excel(
   here("clinical_info/TARGET_AML_ClinicalData_Discovery_20181213.xlsx")
 )
-debug(logger, "Reading COG Data")
+if(exists("logger")) debug(logger, "Reading COG Data")
 cog <- read_excel(here("clinical_info/AAML19B3Q_data_transfer.xlsx"),
   sheet = 2
 ) %>%
@@ -66,10 +65,14 @@ seq <- read_excel(
 
 clinical <- bind_rows(val, dis) %>%
   distinct() %>%
-  separate(`TARGET USI`, into = c(NA, NA, "USI"), sep = "-")
+  separate(`TARGET USI`, into = c(NA, NA, "USI"), sep = "-") %>%
+  sjlabelled::set_na(na = c("N/A", "NA")) %>%
+  select(-`Gemtuzumab ozogamicin treatment`) %>%
+  filter(USI != "paslze" & !is.na(`Bone marrow leukemic blast percentage (%)`))  %>%
+  distinct()
 
 ### TCGA ----
-debug(logger, "Preparing TCGA Data")
+if(exists("logger")) debug(logger, "Preparing TCGA Data")
 tcga_ann <- read_tsv(
   here("data/tcga/clinical.cart.2021-04-22/clinical.tsv"),
   na = "'--",
@@ -105,7 +108,7 @@ tcga_ann2 <- read_tsv(
   )
 
 ### BeatAML ----
-debug(logger, "Preparing BeatAML Data")
+if(exists("logger")) debug(logger, "Preparing BeatAML Data")
 beat_aml_clinical <- read_tsv(
   here("data/aml_ohsu_2018/data_clinical_sample.txt"),
   skip = 4,
