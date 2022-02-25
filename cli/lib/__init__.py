f4e3653221f9dfa080144aa1f771a453a2a94ea8
rschauner <rds156@case.edu>
2022-02-25 13:46:40 -0500

    2 part cli_test -> cli

--- cli/lib/__init__.py
+++
@@ -1,5 +0,0 @@
-# __init__.py
-
-from ...cli_test.lib.cibersort_helpers import concatenate_list_data
-from .invocation_fun import write_invocation
-from ...cli_test.lib.cibersort_helpers import setup_results_dir
--- cli/lib/start_renv.R
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env Rscript
-source('renv/activate.R')
-renv::restore(prompt = FALSE)
